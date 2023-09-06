67f395d522dd77a7dd2fa4fd5414e27c6470489f
hridaya <hridayamishra225@gmail.com>
2023-09-06 15:01:26 +0545

    Adding of Loading and Message Component

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,78 +1,73 @@
 import { useParams, Link } from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button} from "react-bootstrap"
 import Rating from '../components/Rating'
-import axios from "axios";
-import { useState, useEffect, React } from 'react';
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 const ProductScreen = () => {
   const {id: productId} = useParams();
-  const [product, setProduct] = useState({})
-
-  // fetching the products using the id
-  useEffect(()=>{
-    const fetchProduct = async ()=>{
-      const response = await axios.get(`/api/products/${productId}`);
-      setProduct(response.data);
-    }
-    fetchProduct();
-    // const product = products.find((p)=>p._id === productId);
-  }, [productId])
-
-
-  // console.log(product);
-
+  // Fetching the single Product using the redxu and its ID
+  const {data:product, isLoading, error} = useGetProductDetailsQuery(productId);
+  
   return (
     <>
       <Link className='btn btn-light my-3' to={'/'}>Go Back</Link>
+
+      {isLoading? (
+        <Loader/>
+      ): 
+      error ? (
+        <Message variant="danger">{error?.data?.message || error.error}</Message>
+      ):
+      (<>
       <Row>
-        <Col  md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col  md={4}>
+      <Col  md={5}>
+        <Image src={product.image} alt={product.name} fluid />
+      </Col>
+      <Col  md={4}>
+        <ListGroup variant='flush'>
+          <ListGroup.Item>
+            <h3>{product.name}</h3>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Description:</strong> {product.description}
+          </ListGroup.Item>
+        </ListGroup>
+      </Col>
+      <Col  md={3}>
+        <Card>
           <ListGroup variant='flush'>
             <ListGroup.Item>
-              <h3>{product.name}</h3>
+              <Row>
+                <Col>Price:</Col>
+                <Col>
+                  <strong>${product.price}</strong>
+                </Col>
+              </Row>
             </ListGroup.Item>
             <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+              <Row>
+                <Col>Status:</Col>
+                <Col>
+                  <strong>{product.countInStock> 0 ? 'In Stock': "Out Of Stock"}</strong>
+                </Col>
+              </Row>
             </ListGroup.Item>
             <ListGroup.Item>
-              <strong>Description:</strong> {product.description}
+              <Button className='btn-block' type='button' disabled={product.countInStock===0}>
+                Add To Cart
+              </Button>
             </ListGroup.Item>
-            {/* <ListGroup.Item>
-              <strong>
-                Price: ${product.price} 
-              </strong>
-            </ListGroup.Item> */}
           </ListGroup>
-        </Col>
-        <Col  md={3}>
-          <Card>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>{product.countInStock> 0 ? 'In Stock': "Out Of Stock"}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Button className='btn-block' type='button' disabled={product.countInStock===0}>
-                  Add To Cart
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+        </Card>
+      </Col>
+    </Row>
+      </>)}
+
+     
     </>
   )
 }
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -9,8 +9,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    getProductDetails: builder.query({
+      query: (productId)=> ({
+        url: `${PRODUCTS_URL}/${productId}`
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
 // exporting our query 
-export const { useGetProductsQuery } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
